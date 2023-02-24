31397975368b8e363d8fb5340e05f89bc12e6835
vinay1064 <vinaykmr1064@gmail.com>
2023-02-24 11:17:48 +0530

    first commit

---
+++ querry.js
@@ -0,0 +1,47 @@
+// QUERYSELECTOR //
+
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom='solid 32px #ccc';
+
+
+
+// var input =document.querySelector('input');
+// input.value='hello world';
+
+
+// var submit=document.querySelector("input[type='submit']");
+// submit.value='send';
+
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color='red';
+
+
+// var lastitem=document.querySelector('.list-group-item:last-child');
+// lastitem.style.color='blue';
+
+
+
+
+// var seconditem=document.querySelector('.list-group-item:nth-child(2)');
+// seconditem.style.color='pink';
+
+
+
+// queryselectorall //
+
+// var titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent="hello";
+
+
+
+// var odd=document.querySelectorAll('li:nth-child(odd)');
+// var even=document.querySelectorAll('li:nth-child(even)');
+
+// for(var i=0;i<odd.length;i++)
+// {
+//     odd[i].style.backgroundColor="#f4f4f4";
+//     even[i].style.backgroundColor="#ccc";
+// }
+
